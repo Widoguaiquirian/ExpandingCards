d8dd69e70d73d76bc3c1e80a9c5616799fb6af6c
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-12-07 20:57:10 -0300

    New component

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const boxes = document.querySelectorAll(".container__box");
+
+boxes.forEach(function (box) {
+   box.addEventListener("mouseover", function () {
+      removeActive();
+      box.classList.toggle("active");
+   });
+});
+
+const removeActive = function () {
+   boxes.forEach(function (box) {
+      box.classList.remove("active");
+   });
+};
